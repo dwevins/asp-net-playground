26d45076ce15cf92bbca86b1b106401cd1fc8781
Woody <Woody@DESKTOP-82AJ63E>
2022-04-06 14:19:23 -0400

    Add project files.

---
+++ aps-net-playground/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace aps_net_playground.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
